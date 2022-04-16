1546dbc7a1f13b969e3ce00129898af1000e322c
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-04-16 16:49:41 -0300

    Finalizado modulo de EF.

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,34 +14,28 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private IEnumerable<Evento> _evento = new Evento[]
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote="1 Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(5).ToString()
-                },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Temados",
-                Local = "São Paulo",
-                Lote= "2 Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(23).ToString()
-                }
-        };
-        public EventoController()
-        {
-
+            _context = context;
         }
 
         [HttpGet]
+        [Route("")]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos.AsNoTracking().ToList();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Evento Get(int id)
+        {
+            return _context
+                .Eventos
+                .Where(evento => evento.EventoId == id)
+                .AsNoTracking()
+                .SingleOrDefault();
         }
     }
 }
