4e5fb0805097c85769cd6a85c9cad8aef753a649
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-04-24 21:01:11 -0300

    Usando a diretiva ngFor

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventos: any = [
+    {
+      Tema: "Angular",
+      Local: "Belo Horizonte"
+    },
+    {
+      Tema: ".Net 5",
+      Local: "São Paulo"
+    },
+    {
+      Tema: "Angular e suas novidades",
+      Local: "Rio de janeiro"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
