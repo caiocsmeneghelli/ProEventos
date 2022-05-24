31113210b6c8cd1501e90d71ca990784c1b0cd2e
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-05-24 20:49:14 -0300

    Inicio da reformulação do EventosControllers.

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProEventos.Application.Contratos;
 using ProEventos.Domain.Models;
 using ProEventos.Persistence.Contextos;
 
@@ -11,49 +14,90 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        private readonly ProEventosContext _context;
-        public EventosController(ProEventosContext context)
+        private readonly IEventoService _eventoService;
+
+        public EventosController(IEventoService eventoService)
         {
-            _context = context;
+            _eventoService = eventoService;
         }
 
         [HttpGet]
         [Route("")]
-        public IEnumerable<Evento> Get()
+        public async Task<IActionResult> Get()
         {
-            return _context.Eventos.AsNoTracking().ToList();
-        }
-
-        [HttpGet]
-        [Route("{id}")]
-        public Evento Get(int id)
-        {
-            return _context
-                .Eventos
-                .Where(reg => reg.Id == id)
-                .AsNoTracking()
-                .SingleOrDefault();
-        }
-
-        [HttpPost]
-        [Route("")]
-        public void Create([FromBody]Evento model)
-        {
-            if(ModelState.IsValid)
+            try
             {
-                _context.Eventos.Add(model);
-                _context.SaveChanges();
+                var eventos = await _eventoService.GetAllEventosAsync(true);
+                if(eventos == null) return NotFound("Nenhum evento encontrado.");
+                return Ok(eventos);
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
             }
         }
-        
-        [HttpDelete]
+
+        [HttpGet]
         [Route("{id}")]
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> GetById(int id)
         {
-            var evento = _context.Eventos.Single(reg => reg.Id == id);
-            _context.Eventos.Remove(evento);
-            _context.SaveChanges();
-            return Ok();
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(id, true);
+                if(evento == null) 
+                    return NotFound($"Nenhum evento encontrado com o Id: {id}.");
+                return Ok(evento);
+            }
+            catch (System.Exception ex)
+            {
+                 return this.StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"Erro ao tentar recuperar evento. Erro: {ex.Message}");
+            }
         }
+
+        [HttpGet]
+        [Route("tema/{tema}")]
+        public async Task<IActionResult> GetByTema(string tema)
+        {
+            try
+            {
+                var evento = await _eventoService.GetAllEventosByTemaAsync(tema, true);
+                if(evento == null) 
+                    return NotFound($"Nenhum evento encontrado com o Tema: {tema}.");
+                return Ok(evento);
+            }
+            catch (System.Exception ex)
+            {
+                 return this.StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        // [HttpPost]
+        // [Route("")]
+        // public async Task<IActionResult> Create([FromBody]Evento model)
+        // {
+        //     try
+        //     {
+        //         if(!ModelState.IsValid)
+        //             return BadRequest()
+        //     }
+        //     catch (System.Exception)
+        //     {
+                
+        //         throw;
+        //     }
+        // }
+        
+        // [HttpDelete]
+        // [Route("{id}")]
+        // public ActionResult Delete(int id)
+        // {
+        //     var evento = _context.Eventos.Single(reg => reg.Id == id);
+        //     _context.Eventos.Remove(evento);
+        //     _context.SaveChanges();
+        //     return Ok();
+        // }
     }
 }
